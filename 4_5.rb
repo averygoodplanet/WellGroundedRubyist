23af9daa68f08c770f76009f0f5e578a57834b87
averygoodplanet <averygoodplanet@gmail.com>
2014-11-22 18:22:54 -0600

    learning about modules

---
+++ 4_5.rb
@@ -0,0 +1,18 @@
+require 'stacklike'
+
+class Suitcase
+end
+
+class CargoHold
+  include Stacklike
+
+  def load_and_report(obj)
+    print "Loading object "
+    puts obj.object_id
+    add_to_stack(obj)
+  end
+
+  def unload
+    take_from_stack
+  end
+end
