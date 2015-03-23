8c4d035fdadada6c07546193cca21024889359ab
averygoodplanet <averygoodplanet@gmail.com>
2015-03-23 09:36:52 -0500

    5_4 5_5

---
+++ 5_5.rb
@@ -0,0 +1,15 @@
+class C
+  def x(value_for_a, recurse=false)
+    a = value_for_a
+    print "Here's the inspect-string for 'self':"
+    p self
+    puts "And here's a:"
+    puts a
+    if recurse
+      puts "Recursing by calling myself..."
+      x("Second value for a")
+      puts "Back after recursion; here's a:"
+      puts a
+    end
+  end
+end
