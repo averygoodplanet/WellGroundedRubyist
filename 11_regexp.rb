375200155a6f2e8770e0a31cb5a4d8ddd1c61287
averygoodplanet <averygoodplanet@gmail.com>
2016-01-25 18:24:21 -0600

    learn lookahead, lookbehind, (?:foo) regexp

--- 11_regexp.rb
+++ 11_regexp.rb
@@ -128,3 +128,114 @@ irb(main):113:0> match_object.pre_match + match_object[0] + match_object.post_ma
 "the entire body should move together"
 irb(main):127:0> "the entire body should move together"[match_object.begin(0),match_object.end(0) - match_object.begin(0)]
 "body"
+
+# zero-or-one is question mark (?)
+irb(main):001:0> "1".match(/\d/)
+#<MatchData "1">
+irb(main):002:0> "11".match(/\d/)
+#<MatchData "1">
+irb(main):003:0> "11".match(/\d\d?/)
+#<MatchData "11">
+irb(main):004:0> "1".match(/\d\d?/)
+#<MatchData "1">
+
+# the zero-or-more operator is asterisk *  (greedy, match as many as can)
+# example shows that is useful for possible whitespace
+irb(main):006:0> "< /example>".match(/\s*example/)
+#<MatchData "example">
+irb(main):007:0> "< /example   >".match(/\s*example>/)
+nil
+irb(main):008:0> "< /example>".match(/\s*example>/)
+#<MatchData "example>">
+irb(main):009:0> "< /example>".match(/\s*example\s*>/)
+#<MatchData "example>">
+irb(main):010:0> "</example    >".match(/\s*example\s*>/)
+#<MatchData "example    >">
+
+# the one-or-more operator is +  (greedy, match as many as can)
+irb(main):011:0> "Looking for a number 123 here".match(/\d+/)
+#<MatchData "123">
+irb(main):012:0> "Looking for a number here".match(/\d+/)
+nil
+irb(main):013:0> "Looking for a number 123 here".match(/(\d+)/)
+#<MatchData "123" 1:"123">
+irb(main):014:0> $1
+"123"
+
+# making + non-greedy by following it with (?)
+irb(main):015:0> "abc!def!ghi!".match(/.+!/)
+#<MatchData "abc!def!ghi!">
+irb(main):016:0> "abc!def!ghi!".match(/.+?!/)
+#<MatchData "abc!">
+
+# regex engine limits its greediness to trying to get a match
+irb(main):019:0> "limit greed 12345".match(/\d+5/)
+#<MatchData "12345">
+irb(main):020:0> "limit greed 12345".match(/(\d+)5/)
+#<MatchData "12345" 1:"1234">
+irb(main):021:0> "limit greed 12345".match(/(\d+)(5)/)
+#<MatchData "12345" 1:"1234" 2:"5">
+
+# specific number, using {}
+irb(main):022:0> "123-45-6789".match(/(\d{4})/)
+#<MatchData "6789" 1:"6789">
+# range using {a, b}
+irb(main):031:0> "three or more digits, 1234".match(/(\d{3,})/)
+#<MatchData "1234" 1:"1234">
+irb(main):032:0> "three or more digits, 12".match(/(\d{3,})/)
+nil
+
+irb(main):034:0> "between one and two digits, 12".match(/(\d{1,2})/)
+#<MatchData "12" 1:"12">
+irb(main):035:0> "between one and two digits, 1".match(/(\d{1,2})/)
+#<MatchData "1" 1:"1">
+irb(main):036:0> "between one and two digits, 123".match(/(\d{1,2})/)
+#<MatchData "12" 1:"12">
+
+# anchor to beginning of line using caret (^)
+# anchors determine whether a match at all
+irb(main):048:0> "starts with start".match(/^starts.*/)
+#<MatchData "starts with start">
+irb(main):049:0> "doesn't starts with start".match(/^starts.*/)
+nil
+irb(main):050:0> "doesn't starts with start".match(/starts.*/)
+#<MatchData "starts with start">
+
+# see rubular re other anchors
+
+# positive lookahead with (?=foo)
+irb(main):051:0> "123 456. 789".match(/\d+/)
+#<MatchData "123">
+irb(main):052:0> "123 456. 789".match(/\d+\./)
+#<MatchData "456.">
+# positive lookahead checks that it is present but doesn't add it to the match
+irb(main):053:0> "123 456. 789".match(/\d+(?=\.)/)
+#<MatchData "456">
+
+# negative lookahead with (?!foo)
+irb(main):055:0> "123Z 456Z 789A".match(/\d+(?!Z)/)
+#<MatchData "12">
+irb(main):056:0> "123Z 456Z 789A".match(/\d{3}(?!Z)/)
+#<MatchData "789">
+
+#positive lookbehind with (?<=foo)
+irb(main):057:0> "let us prosper".match(/prosper/)
+#<MatchData "prosper">
+irb(main):058:0> "let us prosper".match(/(?<=and )prosper/)
+nil
+irb(main):059:0> "let us live long and prosper".match(/(?<=and )prosper/)
+#<MatchData "prosper">
+
+# negative lookbehind with (?<!foo)
+irb(main):060:0> "a hot dog is not a labrador".match(/(?<!hot )dog/)
+nil
+irb(main):061:0> "a labrador dog is not real dog".match(/(?<!hot )dog/)
+#<MatchData "dog">
+
+# match but don't capture with (?:foo)
+irb(main):066:0> "aaa bbb ccc".match(/(aaa) .* (ccc)/)
+#<MatchData "aaa bbb ccc" 1:"aaa" 2:"ccc">
+irb(main):067:0> "aaa bbb ccc".match(/(aaa) (?:bbb) (ccc)/)
+#<MatchData "aaa bbb ccc" 1:"aaa" 2:"ccc">
+irb(main):068:0> "aaa ddd ccc".match(/(aaa) (?:bbb) (ccc)/)
+nil
